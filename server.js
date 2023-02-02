3a5e9f8ac8a063f1cc7747d775f2c4af1b4e011d
Dowon Kim <dwkim16@gmail.com>
2023-02-01 22:54:18 -0500

    Basic UI Update

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const express = require('express'); //라이브러리 가져오기
-const app = express();
+const express = require('express'); // 방금 설치한 express 라이브러리 참고. express = nodejs용 라이브러리
+const app = express(); // new express object
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.listen(8080, function (){ //서버띄울 포트번호, 띄운 후 실행할 코드
-    console.log('server listening on 8080')
-}); 
+// nodemon: 코드 변경후 저장하면 서버를 껏다 켰다 자동으로 해줌
 
-app.get('/hello', function(req, res){
-    res.send('안녕하세요');
+app.listen(8080, function() { // listen(server port number, 띄운 후 실행할 코드)
+    console.log('server listening on 8080');
+});
+
+// 함수 안에 함수 (function(){}) = 콜백함수 (Call-back function)
+// 순차적으로 실행하고 싶을때 쓴다.
+
+// route 가 '/' 하나만 있는경우 홈페이지
+// .get('경로', function(요청내용, 응답할 방법){})
+app.get('/', function(req, res) { // get(route to get info, function to do after get)
+     res.sendFile(__dirname + '/index.html'); // request, response. 
+     // __dirname: 이 파일이 있는 디렉토리 이름
+});
+
+app.get('/write', function(req, res) { // get(route to get info, function to do after get)
+    res.sendFile(__dirname + '/write.html'); // request, response. 
+    // __dirname: 이 파일이 있는 디렉토리 이름
+});
+
+// 어떤사람이 /add 경로로 POST 요청을 하면 ~~ 해주세요
+app.post('/add', function(req, res){ // 정보는 req에 저장됨.
+    res.send('전송완료');
+    console.log(req.body.title);
 });
