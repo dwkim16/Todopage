05a2783bb7745b0f3a1ea5d231e2526be3dc0fa0
Dowon Kim <dwkim16@gmail.com>
2023-02-07 09:36:43 -0500

    mongodb init

--- server.js
+++ server.js
@@ -5,8 +5,21 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 // nodemon: 코드 변경후 저장하면 서버를 껏다 켰다 자동으로 해줌
 
-app.listen(8080, function() { // listen(server port number, 띄운 후 실행할 코드)
-    console.log('server listening on 8080');
+// var db;
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://dwkim16:12345@dowonmusic.s0uf5.mongodb.net/?retryWrites=true&w=majority', function(error, client){
+    if (error) { return console.log(error); }
+    // 접속이 완료가 되면 콜백 함수 실행
+
+    // db = client.db('todopage');
+
+    // db.collection('post').insertOne('data', function(err, result){
+    //     console.log('saved');
+    // });
+
+    app.listen(8080, function() { // listen(server port number, 띄운 후 실행할 코드)
+        console.log('mongodb server listening on 8080');
+    });
 });
 
 // 함수 안에 함수 (function(){}) = 콜백함수 (Call-back function)
@@ -28,4 +41,5 @@ app.get('/write', function(req, res) { // get(route to get info, function to do 
 app.post('/add', function(req, res){ // 정보는 req에 저장됨.
     res.send('전송완료');
     console.log(req.body.title);
+    console.log(req.body.date);
 });
